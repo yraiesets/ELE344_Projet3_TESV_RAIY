f6b11bfbab686337ba919317356aabf8d51f3fb9
Raies <yasser.raies.1@ens.etsmtl.ca>
2025-04-03 12:14:50 -0400

    Push

--- src/datapath.vhd
+++ src/datapath.vhd
@@ -122,32 +122,111 @@ ARCHITECTURE rtl OF DATAPATH IS
 	SIGNAL MEM_WB_instruction   : STD_LOGIC_VECTOR(31 DOWNTO 0);
 
 BEGIN
-    	-- Mul2-to-1 pour determine l'addresse d'ecriture
-	WriteReg <= Instruction(20 DOWNTO 16) WHEN RegDst = '0' ELSE Instruction(15 DOWNTO 11);
 
-   	 -- Mul2-to-1 pour determine les donnees d'ecriture
-	Result <= ReadData WHEN MemtoReg = '1' ELSE AluResultIntern;
+	-------------------------------------------------------------------------------------
+	--			Etage Pipeline Instruction Fetch (IF)			   --
+	-------------------------------------------------------------------------------------
 
-    	-- Extension de signe (SignExtend)
-	SignImm <= STD_LOGIC_VECTOR(RESIZE(SIGNED(Instruction(15 DOWNTO 0)), N));
+	-- Mul2-to-1 pour determiner IF_PCNextBr
+	IF_PCNextBr <= EX_PCBranch WHEN EX_PCSrc = '1' ELSE IF_PCPlus4;
 
-    	-- Banc de registres
+	-- Mul2-to-1 pour determiner IF_PCNext
+	IF_PCNext <= ID_PCJump WHEN ID_Jump = '1' ELSE IF_PCNextBr;
+
+	-- Bascule D Synchrone avec remise a zero asynchrone (clear).
+	PROCESS(Clk, Reset) IS
+		BEGIN
+			IF Reset = '1' THEN
+				IF_PC <= (OTHERS => '0');
+			ELSIF RISING_EDGE(Clk) THEN
+				IF_PC <= IF_PCNext;
+			END IF;
+	END PROCESS;
+
+	IF_PCPlus4 	<=	STD_LOGIC_VECTOR(UNSIGNED(IF_PC) + TO_UNSIGNED(4, N));
+
+
+	-------------------------------------------------------------------------------------
+	--				Etage Registre IF/ID		   	           --
+	-------------------------------------------------------------------------------------
+
+
+	PROCESS(Clk) BEGIN
+		IF RISING_EDGE(Clk) THEN
+			IF_ID_Instruction	<=	Instruction;
+			IF_ID_PCPlus4		<=	IF_PCPlus4;
+		END IF;	
+	END PROCESS;
+
+	-------------------------------------------------------------------------------------
+	--			Etage Pipeline Instruction Decode (ID)			   --
+	-------------------------------------------------------------------------------------
+	
+	ID_MemtoReg	<=	MemtoReg;
+	ID_MemWrite	<=	MemWriteIn;
+	ID_MemRead	<=	MemReadIn;
+	ID_Branch	<=	Branch;
+	ID_AluSrc	<=	AluSrc;
+	ID_RegDst	<=	RegDst
+	ID_RegWrite	<=	RegWrite;
+	ID_AluControl	<=	AluControl;
+
+	ID_PCJump	<=	IF_ID_PCPlus4(31 DOWNTO 28) & IF_ID_Instruction(25 DOWNTO 0) & "00"; -- Concatenation pour obtenir l'adresse de saut complete
+
+	ID_rs	<= IF_ID_Instruction(25 DOWNTO 21);
+	ID_rt	<= IF_ID_Instruction(20 DOWNTO 16);
+	
+	-- Banc de registres
     	REGISTER_FILE	:	ENTITY work.RegFile(RegFile_arch)
         	PORT MAP(
             		clk	=>	Clk,
-            		we	=>	RegWrite,
-            		ra1	=>	Instruction(25 DOWNTO 21),
-            		ra2	=>	Instruction(20 DOWNTO 16),
-            		wa	=>	WriteReg,
-            		wd	=>	Result,
-            		rd1	=>	rd1,
-            		rd2	=>	rd2
+            		we	=>	MEM_WB_RegWrite,
+            		ra1	=>	ID_rs,
+            		ra2	=>	ID_rt,
+            		wa	=>	MEM_WB_WriteReg,
+            		wd	=>	WB_Result,
+            		rd1	=>	ID_rd1,
+            		rd2	=>	ID_rd2
         	);
 
-	-- Mul2-to-1 pour determine la SrcB de l'ALU
-	SrcB <= SignImm WHEN AluSrc = '1' ELSE rd2;
-	
-	-- UAL
+	ID_rd	<= IF_ID_Instruction(15 DOWNTO 11);
+
+    	-- Extension de signe (SignExtend)
+	ID_SignImm <= STD_LOGIC_VECTOR(RESIZE(SIGNED(IF_ID_Instruction(15 DOWNTO 0)), N));
+
+	-------------------------------------------------------------------------------------
+	--				Etage Registre ID/EX		   	           --
+	-------------------------------------------------------------------------------------
+
+	PROCESS(Clk) BEGIN
+		IF RISING_EDGE(Clk) THEN
+			ID_EX_PCPlus4 	<= IF_ID_PCPlus4;
+			ID_EX_rd1 	<= ID_rd1;
+			ID_EX_rd2	<= ID_rd2;
+			ID_EX_SignImm	<= ID_SignImm;
+			ID_EX_rs	<= ID_rs;
+			ID_EX_rt	<= ID_rt;
+			ID_EX_rd  	<= ID_rd;
+			ID_EX_RegWrite 	<= ID_RegWrite;
+			ID_EX_MemtoReg	<= ID_MemtoReg;
+			ID_EX_MemWrite	<= ID_MemWrite;
+			ID_EX_MemRead 	<= ID_MemRead;
+			ID_EX_Branch	<= ID_Branch;
+		END IF;
+	END PROCESS;
+
+	-------------------------------------------------------------------------------------
+	--			Etage Pipeline Execute (EX)			           --
+	-------------------------------------------------------------------------------------
+
+	-- Determiner si on doit effectuer un branchement
+	EX_PCSrc	<=	ID_EX_Branch AND EX_Zero;
+
+	EX_SignImmSh	<=	STD_LOGIC_VECTOR(SHIFT_LEFT(SIGNED(ID_EX_SignImm), 2));
+
+	EX_PCBranch	<=	STD_LOGIC_VECTOR(SIGNED(ID_EX_PCPlus4) + SIGNED(EX_SignImmSh));
+
+    	-- UAL
 	UAL		:	ENTITY work.UAL(rtl)
 		PORT MAP(
 			ualControl	=>	AluControl,
@@ -155,39 +234,26 @@ BEGIN
 			srcB		=>	SrcB,
 			result		=>	AluResultIntern,
 			cout		=>	OPEN,
-			zero		=>	Zero
+			zero		=>	EX_Zero
 		);
 
-	-- Determiner si on doit effectuer un branchement
-	PCSrc		<=	Branch AND Zero;
+	-- Forwading Unit
+
+    	-- Mul2-to-1 pour determine l'addresse d'ecriture
+	WriteReg <= Instruction(20 DOWNTO 16) WHEN RegDst = '0' ELSE Instruction(15 DOWNTO 11);
+
+   	 -- Mul2-to-1 pour determine les donnees d'ecriture
+	Result <= ReadData WHEN MemtoReg = '1' ELSE AluResultIntern;
+
+
+	-- Mul2-to-1 pour determine la SrcB de l'ALU
+	SrcB <= SignImm WHEN AluSrc = '1' ELSE rd2;
 	
-	-- Calcul des signaux utilises dans la logique du PC :
-	PCPlus4 	<=	STD_LOGIC_VECTOR(UNSIGNED(PCIntern) + TO_UNSIGNED(4, N));
-	PCJump		<=	PCPlus4(31 DOWNTO 28) & Instruction(25 DOWNTO 0) & "00"; -- Concatenation pour obtenir l'adresse de saut complete
-	SignImmSh	<=	STD_LOGIC_VECTOR(SHIFT_LEFT(SIGNED(SignImm), 2));
-	PCBranch	<=	STD_LOGIC_VECTOR(SIGNED(PCPlus4) + SIGNED(SignImmSh));
-
-	-- Mul2-to-1 pour determiner PCNextBr
-	PCNextBr <= PCBranch WHEN PCSrc = '1' ELSE PCPlus4;
-
-
-	-- Mul2-to-1 pour determiner PCNext
-	PCNext <= PCJump WHEN Jump = '1' ELSE PCNextBr;
-
-	-- Bascule D Synchrone avec remise a zero asynchrone (clear).
-	PROCESS(Clk, Reset) IS
-		BEGIN
-			IF Reset = '1' THEN
-				PCIntern <= (OTHERS => '0');
-			ELSIF RISING_EDGE(Clk) THEN
-				PCIntern <= PCNext;
-			END IF;
-	END PROCESS;
 
 	-- Assignation des Sorties
 	MemReadOut	<=	MemReadIn;
 	MemWriteOut	<=	MemWriteIn;
-	PC		<=	PCIntern;
+	PC		<=	IF_PC;
 	AluResult	<=	AluResultIntern;
 	WriteData	<=	rd2;
 	
